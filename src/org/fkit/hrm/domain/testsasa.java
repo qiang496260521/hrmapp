027c0d90548f63cf86d9c0a600978b4907d0c511
qiangge <496260521@qq.com>
2018-11-21 01:04:17 +0800

    asdas

---
+++ src/org/fkit/hrm/domain/testsasa.java
@@ -0,0 +1,19 @@
+package org.fkit.hrm.domain;
+
+public class testsasa {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "testsasa [name=" + name + "]";
+	}
+	
+}
